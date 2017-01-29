ba2d763cf441adf6a3587cdddfea6e14a217c2c6
Juan Mac <juanf03@gmail.com>
2017-01-29 04:25:33 -0300

    json campaign activity created and added firebase ui and firebase pods

--- donors_tweet_ios/ViewController.swift
+++ donors_tweet_ios/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var campaignRowsData  = ["Campaign 1","Campaign 2","Campaign 3'"]
+    
+    var serverFetchCampaignsUrl = "http://localhost:3000/api/campaigns/get/all"
+    
+    
+    @IBOutlet weak var campaignTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        campaignTableView.delegate=self
+        
+        campaignTableView.dataSource=self
+        getCampaignList()
     }
+    
+    
+    func getCampaignList(){
+    
+        Alamofire.request(serverFetchCampaignsUrl, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                //print("JSON: \(json)")
+                
+                let campaigns =  json["rows"].arrayValue.map({
+                    
+                
+                    //create campaign cards here
+                    
+                    print($0["cause_description"].stringValue)
+                
+                
+                })
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return campaignRowsData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = campaignTableView.dequeueReusableCell(withIdentifier: "CampaignCell")
+    cell?.textLabel?.text = campaignRowsData[indexPath.row]
+        
+        return cell!
+    }
+    
+    
 
 }
 
