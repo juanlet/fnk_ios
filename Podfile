b373e1896b3460d437c44941be92b99ab2b28e3b
Juan Mac <juanf03@gmail.com>
2017-01-29 02:25:21 -0300

    $1

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'donors_tweet_ios' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for donors_tweet_ios
+
+    pod 'Alamofire', '~> 4.3'
+    pod 'SDWebImage', '~>3.8'
+    pod 'SwiftyJSON'
+    pod 'SVProgressHUD'
+    pod 'SnapKit', '~> 3.1.2'
+    pod 'Locksmith'
+
+
+
+
+end
---
+++ donors_tweet_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  donors_tweet_ios
+//
+//  Created by Juan andrés Ferreyra on 1/28/17.
+//  Copyright © 2017 shamanito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
