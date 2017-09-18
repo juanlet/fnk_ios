1285fac5a60d17bcd65f5e74feb568118ba59c37
Juan Mac <juanf03@gmail.com>
2017-09-17 22:06:59 -0300

    sponsor card set

--- donors_tweet_ios/CampaignDetailsViewController.swift
+++ donors_tweet_ios/CampaignDetailsViewController.swift
@@ -158,21 +158,31 @@ class CampaignDetailsViewController: UIViewController {
         sponsorTwitterNameLabel.text = sponsorTwitterUsername != "" ?  sponsorTwitterUsername : ""
         
         let sponsorName = adCampaignData["name"].stringValue
+        
         sponsorNameLabel.text = sponsorName != "" ? sponsorName : ""
         
         let sponsor_id = adCampaignData["sponsor_id"].stringValue
         
+        
+        
         let allTimeUsersReached = adCampaignData["all_time_users_reached"].int
         
-        let sponsorPicUrl = URL(string: adCampaignData["pic_url"].stringValue)
-            
-    
+        
+        
+        let sponsorPicUrlString = adCampaignData["pic_url"].stringValue
+        let sponsorPicUrl = URL(string: sponsorPicUrlString)
+        
+        if(sponsorPicUrl != nil){
+             self.setImageViewAsync(sponsorPicUrl!, imageView: sponsorLogoImageView)
+        }
+        
+        
         
          //ad
         
-         let ad = adCampaignData["ad"]
+        let ad = adCampaignData["ad"]
         
-         let sponsorAdId = ad["sponsor_ad_id"].stringValue
+        let sponsorAdId = ad["sponsor_ad_id"].stringValue
         
         let bannerUrlString = ad["banner_url"].stringValue
         
@@ -184,22 +194,49 @@ class CampaignDetailsViewController: UIViewController {
         let tweetTextContent = ad["text"].stringValue
         
         
+        adTextLabel.text = tweetTextContent
+        
+        
          //youtube video data
         
          let video = adCampaignData["youtube_video"]
         
          let videoTitle = video["title"].stringValue
+        
+         adVideoNameLabel.text = videoTitle
+        
          let videoDescription = video["description"].stringValue
+        
+         adVideoDescriptionLabel.text = videoDescription
+        
          let videoThumbnailUrlString = video["thumbnail"]["url"].stringValue
-         let videoThumbnailUrl:URL = videoThumbnailUrlString != "" ? URL(string: videoThumbnailUrlString) : nil
+        
+         let videoThumbnailUrl:URL? = videoThumbnailUrlString != "" ? URL(string: videoThumbnailUrlString) : nil
+        
+        if videoThumbnailUrl != nil{
+         self.setImageViewAsync(videoThumbnailUrl!, imageView: adVideoThumbnailImageView)
+        }
          let videoWidth = video["thumbnail"]["width"].int
          let videoHeight = video["thumbnail"]["height"].int
         
         
-         
+        
          
 
 
+        
+    }
+    
+    
+    
+    func setImageViewAsync(_ imageUrl:URL, imageView:UIImageView){
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: imageUrl) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            DispatchQueue.main.async {
+                imageView.image = UIImage(data: data!)
+            }
+        }
         
     }
 
